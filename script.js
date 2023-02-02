34d43a8747db416ebaa06c25d90ccd9be901f11a
100日後にプログラミングで稼ぐ大学生｜ミワ_Miwa <121470211+Miwa-programming-and-AIweb@users.noreply.github.com>
2023-02-02 17:40:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,51 @@ target.textContent = year + '年' + month + '月' + date + `日`;
 
 
 
-
-// 目次自動生成
+// 目次自動生成--JavaScriptを学んだら、htmlとcssとコレを変えて、<i class="fa-solid fa-circle-chevron-right"></i>を使う。
 document.addEventListener('DOMContentLoaded', () => {
     const heads = document.querySelectorAll('h2, h3');
     if (heads && heads.length) {
         let contents = '';
         heads.forEach((head, i) => {
-            contents += `<li><a href="#head${i}">${head.textContent}</a></li>`;
+            let className = '';
+            switch (head.localName) {
+                case "h2":
+                    className = `contents2`;
+                    break;
+                case "h3":
+                    className = `contents3`;
+                    break;
+            }
+            contents += `<li><a class="${className}" href="#head${i}">${head.textContent}</a></li>`;
             head.innerHTML += `<a id="head${i}"></a>`;
         })
         document.querySelector('#toc').innerHTML += `<ul>${contents}</ul>`;
     }
 });
+
+
+
+// 本ページのURLを取得
+let thisPageURL = location.href;
+console.log(thisPageURL);
+
+
+
+
+//ハンバーガーメニューの開閉
+var headerNav = document.getElementById("header-nav");
+var clickNumber = 0
+
+window.addEventListener("DOMContentLoaded", function(){
+    headerNav.style.cssText="opacity:0;";
+})
+
+document.getElementById("header-hamburger").onclick = function () {
+    clickNumber++;
+
+    if (clickNumber % 2 === 1) {
+        headerNav.style.cssText = "z-index:5;opacity:1; transition:opacity .5s ease 0s;";
+    } else {
+        headerNav.style.cssText = "opacity:0;";
+    }
+}
